2b54f93593417b99166c7d68a257f74d62207d97
SrtaBecerril <jcarrascobecerril@gmail.com>
2024-03-11 04:30:38 +0100

    commit apis 2: importando API de Clima

--- judith-proyecto-angular/src/app/app.routes.ts
+++ judith-proyecto-angular/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ClimaComponent } from './pages/clima/clima.component';
 
 export const routes: Routes = [
 
-    {path: '', component:HomeComponent},
+    {path: '', component:HomeComponent, title :'Inicio'},
+    {path: 'clima', component:ClimaComponent, title :'Clima'},
     {path: '**',redirectTo: '',pathMatch:'full' },
-
 ];
 
