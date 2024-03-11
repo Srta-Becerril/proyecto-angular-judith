a6cd0987b4e29cf1a7cdb99497be5170ba1e0984
SrtaBecerril <jcarrascobecerril@gmail.com>
2024-03-11 03:49:08 +0100

    primer commit

---
+++ judith-proyecto-angular/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: 
+  [
+    
+    HeaderComponent,
+    SidebarComponent,
+    RouterOutlet,
+    FooterComponent,
+   
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'grid-proyect';
+}
+
+
