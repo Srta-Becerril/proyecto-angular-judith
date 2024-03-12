d776f8a4e7299651d566ae6cf0d875a080645ab4
SrtaBecerril <jcarrascobecerril@gmail.com>
2024-03-12 10:43:34 +0100

    commit varios 8

--- judith-proyecto-angular/src/app/app.routes.ts
+++ judith-proyecto-angular/src/app/app.routes.ts
@@ -1,17 +1,17 @@
 // MÓDULOS IMPORTADOS
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 import { Routes } from '@angular/router'; // Importar RouterModule
 import { HomeComponent } from './pages/home/home.component'; // Importar componente home
 import { ClimaComponent } from './pages/clima/clima.component'; // Importar componente clima
 import { githubComponent } from './pages/github/github.component'; // Importar componente GitHub
 import { NasaComponent } from './pages/nasa/nasa.component'; // Importar componente NASA
 import { NgModule } from '@angular/core'; // Importar NgModule
-import { FormsModule } from '@angular/forms'; // Importar FormsModule
-import { FullCalendarModule } from '@fullcalendar/angular'; // Importar FullCalendar
 import { GestionArchivosComponent } from './components/gestion-archivos/gestion-archivos.component'; // Importar componente GestionArchivos
 import { CameraComponent } from './components/camera/camera.component'; // Importar componente Camera
 import { WindowComponent } from './components/window/window.component'; // Importar componente Window
 import { CalculadoraComponent } from './components/calculadora/calculadora.component'; // Importar componente Calculadora
 import { RelojMundialComponent } from './components/reloj/reloj.component'; // Importar componente Reloj
+import { CommonModule } from '@angular/common'; // Importar CommonModule
 
 // RUTAS
 export const routes: Routes = [
@@ -27,12 +27,9 @@ export const routes: Routes = [
     { path: '**', redirectTo: '', pathMatch: 'full' } // Ruta por defecto
 ];
 
-// MODULO
-import { CommonModule } from '@angular/common';
-
 // COMPONENTE NgModule
 @NgModule({
   declarations: [NasaComponent, GestionArchivosComponent, CameraComponent, WindowComponent, CalculadoraComponent, RelojMundialComponent], // Componentes declarados
-  imports: [CommonModule, FormsModule, FullCalendarModule], // Módulos importados
+  imports: [CommonModule, FormsModule], // Módulos importados
 })
 export class OtroModulo { } // Exportar componente
